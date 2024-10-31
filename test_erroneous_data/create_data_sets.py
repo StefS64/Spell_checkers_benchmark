5a4f4a444d75905c04df5e91b3fd18a2033213d8
StefS64 <stefan.swierczewski@gmail.com>
2024-10-31 13:33:06 +0100

    standardized input script work in progress for creating data sets

--- test_erroneous_data/create_data_sets.py
+++ test_erroneous_data/create_data_sets.py
@@ -1,20 +1,33 @@
 import json
 import os
+# TODO Create a user interaction system (maybe don't create all of data sets but only some).
+# TODO Right now just create a working benchmark for one directory.
+# TODO If multiple sources of data will be used standarize tha data.
+
+
 
 # Specify the directory containing your JSON files
-directory_path = './test_erroneous_data/raw_data/single_words' 
+directory_path = './test_erroneous_data/standardized_input/single_words' 
 
 # Dictionary to store all loaded JSON data, keyed by filename
 correct_word_dictionaries = {}
 
-# Loop through each file in the directory
+# Get valid filenames
+valid_filenames = []
 for filename in os.listdir(directory_path):
-    if filename.endswith('.json'): 
-        file_path = os.path.join(directory_path, filename)
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-            correct_word_dictionaries[filename] = data
+    if filename.endswith('.json'):
+        valid_filenames.append(filename)
+
+
+# Loop through each file in the directory
+for filename in valid_filenames:
+    file_path = os.path.join(directory_path, filename)
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        correct_word_dictionaries[filename] = data
 
 # Now 'correct_word_dictionaries' contains all JSON contents, accessible by filename
-print(correct_word_dictionaries[:5])
+print(correct_word_dictionaries)
 
+for filename in valid_filenames:
+    
